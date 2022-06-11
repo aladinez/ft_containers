22233056a84f1bd1f277d4b6b8e3567560efef30
aladinez <informaticienalae@gmail.com>
2022-06-11 23:13:03 +0100

    a repo to test the STL containers implemented

---
+++ ft_containers-unit-test/config.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+/**************************************************/
+/*			Change paths to your files			  */
+/*				or comment #include				  */
+/*		if you haven't yet some container		  */
+/**************************************************/
+#define VECTOR			"../../../vector.hpp"
+#define STACK			"../../../Stack.hpp"
+#define MAP			  	"../../../Map.hpp"
+#define SET			  	"../../../Set.hpp"
+#define UTILITIES		"../../../Utility.hpp"
+
+/* comment this to turn OFF flags (-WWW, -std=98) */
+// #define FLAGS
+
+/**************************************************/
+/*		Change template names of your classes	  */
+/**************************************************/
+#define	_vector 			ft::vector
+#define	_stack	 			ft::Stack
+#define	_map 				ft::Map
+#define	_set	 			ft::Set
+#define	_is_integral 		ft::is_integral
+#define	_enable_if 			ft::enable_if
+#define	_pair 				ft::pair
+#define	_make_pair 			ft::make_pair
+#define	_lexicographical	ft::lexicographical_compare
+#define	_equal				ft::equal
